783116085689ef37f011b4bc6d0395b51e96ca3a
Bjørn Sørensen <bso@lunar.app>
2021-09-17 14:19:16 +0200

    Extract into consts

--- internal/operator/operator.go
+++ internal/operator/operator.go
@@ -7,24 +7,28 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-const InjectionAnnotation = "config.lunar.tech/cluster-identity-inject"
+const (
+	InjectionAnnotation = "config.lunar.tech/cluster-identity-inject"
+
+	kubeControllerManagerContainerName           = "kube-controller-manager"
+	kubeControllerManagerContainerArgumentPrefix = "--cluster-name="
+)
 
 // ClusterNameFromPod extracts the cluster name from a kube-controller-manager
 // Pod definition.
 func ClusterNameFromPod(pod *corev1.Pod) (string, error) {
-	containerName := "kube-controller-manager"
 	for _, container := range pod.Spec.Containers {
-		if container.Name != containerName {
+		if container.Name != kubeControllerManagerContainerName {
 			continue
 		}
-		argumentPrefix := "--cluster-name="
-		name := find(container.Args, argumentPrefix)
+
+		name := find(container.Args, kubeControllerManagerContainerArgumentPrefix)
 		if name == "" {
-			return "", fmt.Errorf("could not find --cluster-name flag in container '%s'", container.Name)
+			return "", fmt.Errorf("could not find '%s' flag in container '%s'", kubeControllerManagerContainerArgumentPrefix, container.Name)
 		}
-		return strings.TrimPrefix(name, argumentPrefix), nil
+		return strings.TrimPrefix(name, kubeControllerManagerContainerArgumentPrefix), nil
 	}
-	return "", fmt.Errorf("could not find container with name '%s'", containerName)
+	return "", fmt.Errorf("could not find container with name '%s'", kubeControllerManagerContainerName)
 }
 
 func find(stack []string, needle string) string {
